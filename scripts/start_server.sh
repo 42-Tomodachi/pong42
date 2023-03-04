f167282c00142035b3e161e018215812f364de79
jihoon <jihokim@student.42seoul.kr>
2023-03-04 23:38:40 +0900

    Fix: add database preserving lines

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,7 @@
 
 cp /home/ubuntu/.env.backend /home/ubuntu/pong42_server/backend/.env
 cp /home/ubuntu/.env.database /home/ubuntu/pong42_server/database/.env
+mv /home/ubuntu/pgdata /home/ubuntu/pong42_server/database/.env/home/ubuntu/pong42_server/database/pgdata
 
 cd /home/ubuntu/pong42_server
 docker-compose up --build

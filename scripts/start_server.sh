ca20499eac081c401d957147e90b6d3f36b515df
jihoon <jihokim@student.42seoul.kr>
2023-03-04 23:55:34 +0900

    Fix: add backend clean-up

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,7 +2,7 @@
 
 cp /home/ubuntu/.env.backend /home/ubuntu/pong42_server/backend/.env
 cp /home/ubuntu/.env.database /home/ubuntu/pong42_server/database/.env
-sudo mv /home/ubuntu/pgdata /home/ubuntu/pong42_server/database/pgdata
+mv /home/ubuntu/files /home/ubuntu/pong42_server/backend/files
 
 cd /home/ubuntu/pong42_server
 docker-compose up --build
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 rm /home/ubuntu/pong42_server/backend/.env
+rm /home/ubuntu/pong42_server/backend/package-lock.json
+rm -rf /home/ubuntu/pong42_server/backend/dist
+rm -rf /home/ubuntu/pong42_server/backend/node_modules
+mv /home/ubuntu/pong42_server/backend/files /home/ubuntu/
+
 rm /home/ubuntu/pong42_server/database/.env
 sudo mv /home/ubuntu/pong42_server/database/pgdata /home/ubuntu/
 

9be01fa9d97e4c639ebae749495bca4106a3f396
jihoon <jihokim@student.42seoul.kr>
2023-03-04 23:45:19 +0900

    Fix: permission matter

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,7 +2,7 @@
 
 rm /home/ubuntu/pong42_server/backend/.env
 rm /home/ubuntu/pong42_server/database/.env
-mv /home/ubuntu/pong42_server/database/.env/home/ubuntu/pong42_server/database/pgdata /home/ubuntu/
+sudo mv /home/ubuntu/pong42_server/database/pgdata /home/ubuntu/
 
 cd /home/ubuntu/pong42_server
 docker-compose stop

2946f2e4bd2fb127bfa0aff852b23d10715c09cd
jihoon <jihokim@student.42seoul.kr>
2023-03-04 22:49:17 +0900

    Fix: add handling on .env files

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+cp /home/ubuntu/.env.backend /home/ubuntu/pong42_server/backend/.env
+cp /home/ubuntu/.env.database /home/ubuntu/pong42_server/database/.env
+
 cd /home/ubuntu/pong42_server
 docker-compose up --build
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+rm /home/ubuntu/pong42_server/backend/.env
+rm /home/ubuntu/pong42_server/database/.env
+
 cd /home/ubuntu/pong42_server
 docker-compose stop
 docker system prune --force

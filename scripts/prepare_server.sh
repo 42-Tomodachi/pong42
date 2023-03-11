edbc67fe4008cfb3cdd67ada868e45ccada0368e
Jihoon Kim <kjhgomtang2@naver.com>
2023-03-11 21:13:57 +0900

    Fix: modification on appspec..

---
+++ scripts/prepare_server.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cp /home/ubuntu/.env.backend /home/ubuntu/pong42_server/backend/.env
+cp /home/ubuntu/.env.database /home/ubuntu/pong42_server/database/.env
+mv /home/ubuntu/files /home/ubuntu/pong42_server/backend/files
+
+cd /home/ubuntu/pong42_server
+
+echo "Build docker images..."
+docker-compose build
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-cp /home/ubuntu/.env.backend /home/ubuntu/pong42_server/backend/.env
-cp /home/ubuntu/.env.database /home/ubuntu/pong42_server/database/.env
-mv /home/ubuntu/files /home/ubuntu/pong42_server/backend/files
-
-cd /home/ubuntu/pong42_server
-docker-compose up --build
+echo "Starting Containers..."
+docker-compose up

6ecf11f07d99c7befe52dbd4fbc574e4d46011e8
Jihoon Kim <kjhgomtang2@naver.com>
2023-04-18 16:39:29 +0900

    Fix: fix gitaction workflow

--- frontend/src/constVariables.ts
+++ frontend/src/constVariables.ts
@@ -6,7 +6,7 @@
 // cat << EOF > ./src/constVariables.ts
 // export const serverUrl = 'localhost:5500';
 // export const apiKey = 'someGoodLongComlicatedHashsum';
-// export const OauthUrl = `https://api.intra.42.fr/oauth/blahblah`;
+// export const OauthUrl = \`https://api.intra.42.fr/oauth/blahblah\`;
 // EOF
 //
 

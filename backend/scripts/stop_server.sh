951048c24065e58cf7726b3cf9362ed266ddb16a
Jihoon Kim <kjhgomtang2@naver.com>
2023-04-18 15:03:22 +0900

    Fix: script fix for renaming

--- backend/scripts/stop_server.sh
+++ backend/scripts/stop_server.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-rm ~/pong42_server/backend/.env
-rm ~/pong42_server/backend/package-lock.json
-rm -rf ~/pong42_server/backend/dist
-sudo rm -rf ~/pong42_server/backend/node_modules
-mv ~/pong42_server/backend/files ~/files_backup
+rm ~/pong42_server/server/.env
+rm ~/pong42_server/server/package-lock.json
+rm -rf ~/pong42_server/server/dist
+sudo rm -rf ~/pong42_server/server/node_modules
+mv ~/pong42_server/server/files ~/files_backup
 
 rm ~/pong42_server/database/.env
 sudo rm -rf ~/pgdata_backup
